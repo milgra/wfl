ec5524a9be8f1024335a31c32aa6b71b0dfbabb8
Milan Toth <milgra@milgra.com>
2023-12-10 15:14:34 +0100

    wfl update

--- wfl.sh
+++ wfl.sh
@@ -18,35 +18,23 @@ kuid -v <&3 | while IFS= read -r line; do
 	    echo "create layer width 300 height 295 anchor lt margin 10" >&3
 	    echo "load html src ~/.config/wfl/res/main.html" >&3
 
-	    echo "set text div item0label value GlobalProtect" >&3
+	    echo "set text div item0label value Terminal" >&3
 	    echo "set text div item0number value 0" >&3
-	    echo "set text div item1label value Citrix" >&3
+	    echo "set text div item1label value Nautilus" >&3
 	    echo "set text div item1number value 1" >&3
-	    echo "set text div item2label value Terminal" >&3
+	    echo "set text div item2label value Chromium" >&3
 	    echo "set text div item2number value 2" >&3
-	    echo "set text div item3label value Nautilus" >&3
+	    echo "set text div item3label value LibreOffice" >&3
 	    echo "set text div item3number value 3" >&3
-	    echo "set text div item4label value Chromium" >&3
+	    echo "set text div item4label value MMFM" >&3
 	    echo "set text div item4number value 4" >&3
-	    echo "set text div item5label value LibreOffice" >&3
+	    echo "set text div item5label value VMP" >&3
 	    echo "set text div item5number value 5" >&3
-	    echo "set text div item6label value MMFM" >&3
-	    echo "set text div item6number value 6" >&3
-	    echo "set text div item7label value VMP" >&3
-	    echo "set text div item7number value 7" >&3
 
 	# button events
 	elif [ ${words[1]} == "state" ]
 	then
 	    if [ ${words[3]} == "item0labelback" ]
-	    then
-		swaymsg exec globalprotect launch-ui
-		echo "toggle visibility" >&3
-	    elif [ ${words[3]} == "item1labelback" ]
-	    then
-		swaymsg exec /opt/Citrix/ICAClient/selfservice
-		echo "toggle visibility" >&3
-	    elif [ ${words[3]} == "item2labelback" ]
 	    then
 		swaymsg exec foot
 		echo "toggle visibility" >&3
@@ -69,10 +57,6 @@ kuid -v <&3 | while IFS= read -r line; do
 	    elif [ ${words[3]} == "item7labelback" ]
 	    then
 		swaymsg exec vmp
-		echo "toggle visibility" >&3
-	    elif [ ${words[3]} == "item8labelback" ]
-	    then
-		swaymsg exec system-config-printer
 		echo "toggle visibility" >&3
 	    fi
 	fi
